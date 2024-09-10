f07e7caf0f19265274721521840d69f7333607ca
joão <juaun1981@alu.ufc.br>
2024-09-10 08:41:13 -0300

    alteração do metodo findPostByTitle para com a annotation @Querry

--- src/main/java/com/example/mongodb/repositories/PostRepository.java
+++ src/main/java/com/example/mongodb/repositories/PostRepository.java
@@ -2,6 +2,7 @@ package com.example.mongodb.repositories;
 
 import com.example.mongodb.domain.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,5 +11,8 @@ import java.util.List;
 public interface PostRepository extends MongoRepository<Post, String>{
 
     List<Post> findByTitleContainingIgnoreCase(String text);
+
+    @Query("{ 'title': { $regex:  ?0, $options: 'i'} }")
+    List<Post> findPostByTitle(String text);
 
 }
--- src/main/java/com/example/mongodb/services/PostService.java
+++ src/main/java/com/example/mongodb/services/PostService.java
@@ -22,7 +22,7 @@ public class PostService {
     }
 
     public List<Post> findPostByTitle(String text) {
-        return postRepository.findByTitleContainingIgnoreCase(text);
+        return postRepository.findPostByTitle(text);
     }
 
 }
